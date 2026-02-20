5c7df67bc372df2fc85360f44ad42b2923af5ea8
kiril <khorosko15@gmail.com>
2026-02-20 15:58:42 +0300

    feat: add telegram client

---
+++ src/LinkTracker.Bot/Telegram/ITelegramClient.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types;
+
+namespace LinkTracker.Bot.Telegram;
+
+public interface ITelegramClient
+{
+    Task SendMessageAsync(long chatId, string message);
+    Task SetCommandsAsync(IEnumerable<BotCommand> commands);
+}
