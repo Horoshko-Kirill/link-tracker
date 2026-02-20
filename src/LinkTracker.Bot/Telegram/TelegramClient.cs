1a8589f216bf734c9bf7696975c6f3558e58b0f2
kiril <khorosko15@gmail.com>
2026-02-20 15:58:42 +0300

    feat: add telegram client

---
+++ src/LinkTracker.Bot/Telegram/TelegramClient.cs
@@ -0,0 +1,29 @@
+﻿using LinkTracker.Bot.Configuration;
+using Microsoft.Extensions.Options;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LinkTracker.Bot.Telegram;
+
+public class TelegramClient : ITelegramClient
+{
+    private readonly TelegramBotClient _client;
+    private readonly ILogger<TelegramClient> _logger;
+    public TelegramClient(IOptions<BotOptions> options, ILogger<TelegramClient> logger)
+    {
+        _client = new TelegramBotClient(options.Value.Token);
+        _logger = logger;
+    }
+
+    public async Task SendMessageAsync(long chatId, string message)
+    {
+        _logger.LogInformation("Sending message to {ChatId}", chatId);
+        await _client.SendMessage(chatId, message);
+    }
+
+    public async Task SetCommandsAsync(IEnumerable<BotCommand> commands)
+    {
+        await _client.SetMyCommands(commands);
+    }
+}
