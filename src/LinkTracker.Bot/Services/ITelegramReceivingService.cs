7acad84d1ae502a468e36800fa49071b94d6d2e4
kiril <khorosko15@gmail.com>
2026-03-02 14:29:36 +0300

    fix: Hosted service update

--- src/LinkTracker.Bot/Services/ITelegramReceivingService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LinkTracker.Bot.Services;
-
-/// <summary>
-/// Интерфейс сервиса для передачи команд телеграмму и назначения обработчика обновлений
-/// </summary>
-public interface ITelegramReceivingService
-{
-    public Task StartAsync(CancellationToken cancellationToken);
-}
--- src/LinkTracker.Bot/Services/TelegramHostedService.cs
+++ src/LinkTracker.Bot/Services/TelegramHostedService.cs
@@ -1,23 +1,49 @@
-﻿namespace LinkTracker.Bot.Services;
+﻿using LinkTracker.Bot.Commands.Interfaces;
+using LinkTracker.Bot.Dispatching;
+using LinkTracker.Bot.Telegram;
+using Telegram.Bot.Types.Enums;
+
+namespace LinkTracker.Bot.Services;
 /// <summary>
 /// Класс, запускающий фоновую задачу для отслеживания обновлений телеграма 
 /// </summary>
 public class TelegramHostedService : IHostedService
 {
-    private readonly ITelegramReceivingService _telegramReceivingService;
+    private readonly ITelegramClient _client;
+    private readonly ICommandDispatcher _dispatcher;
+    private readonly IEnumerable<ICommand> _commands;
 
-    public TelegramHostedService(ITelegramReceivingService telegramReceivingService)
+    private CancellationTokenSource? _cts;
+    public TelegramHostedService(
+        ITelegramClient client,
+        ICommandDispatcher dispatcher,
+        IEnumerable<ICommand> commands)
     {
-        _telegramReceivingService = telegramReceivingService;
+        _client = client;
+        _dispatcher = dispatcher;
+        _commands = commands;
     }
 
-    public Task StartAsync(CancellationToken cancellationToken = default)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        return _telegramReceivingService.StartAsync(cancellationToken);
+        _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+        await _client.SetCommandsAsync(_commands, _cts.Token);
+
+        _client.StartReceivingAsync(async (update) =>
+        {
+            if (update.Type != UpdateType.Message || update.Message?.Text is null)
+            {
+                return;
+            }
+
+            await _dispatcher.DispatchAsync(update.Message.Text, update.Message.Chat.Id, _cts.Token);
+        }, _cts.Token);
     }
 
-    public Task StopAsync(CancellationToken cancellationToken = default)
+    public Task StopAsync(CancellationToken cancellationToken)
     {
+        _cts?.Cancel();
         return Task.CompletedTask;
     }
 }
