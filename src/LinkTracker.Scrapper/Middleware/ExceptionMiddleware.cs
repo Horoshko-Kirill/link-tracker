4c7c87cebe366ab13f455b5e513f855a1a5f178e
kiril <khorosko15@gmail.com>
2026-03-05 01:14:10 +0300

    feat: add middleware exception handler

---
+++ src/LinkTracker.Scrapper/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using LinkTracker.Scrapper.Application.Exceptions;
+using LinkTracker.Scrapper.Contracts.Dto;
+
+namespace LinkTracker.Scrapper.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(context, ex);
+        }
+    }
+
+    private static async Task HandleException(HttpContext context, Exception ex)
+    {
+        var response = new ApiErrorResponse
+        {
+            Code = ex.GetType().Name,
+            ExceptionName = ex.GetType().Name,
+            ExceptionMessage = ex.Message,
+            StackTrace = ex.StackTrace?.Split('\n').ToList()
+        };
+
+        context.Response.ContentType = "application/json";
+
+        context.Response.StatusCode = ex switch
+        {
+            BadRequestException => 400,
+            ConflictException => 409,
+            NotFoundException => 404,
+            _ => 500
+        };
+
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
