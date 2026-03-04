e1109d2abed59f4f9a80278d0bb1cd84727262da
kiril <khorosko15@gmail.com>
2026-03-05 01:46:08 +0300

    feat: add scrapper controllers

---
+++ src/LinkTracker.Scrapper/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using LinkTracker.Scrapper.Application.InterfacesServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkTracker.Scrapper.Controllers;
+
+[ApiController]
+[Route("tg-chat")]
+public class ChatController : ControllerBase
+{
+    private readonly IChatService _chatService;
+
+    public ChatController(IChatService chatService)
+    {
+        _chatService = chatService;
+    }
+
+    [HttpPost("{id:long}")]
+    public async Task<IActionResult> RegisterChat([FromRoute] long id, CancellationToken cancellationToken = default)
+    {
+        await _chatService.RegisterChatAsync(id, cancellationToken);
+        return Ok();
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<IActionResult> DeleteChat([FromRoute] long id, CancellationToken cancellationToken = default)
+    {
+        await _chatService.DeleteChatAsync(id, cancellationToken);
+        return Ok();
+    }
+}
