ccb9990ece31ca3c96742a8eead7dd18747f2d8c
kiril <khorosko15@gmail.com>
2026-03-04 03:10:38 +0300

    feat: add repository

---
+++ src/LinkTracker.Scrapper.Application/InterfacesRepositories/IChatRepository.cs
@@ -0,0 +1,11 @@
+﻿using LinkTracker.Scrapper.Domain.Models;
+
+namespace LinkTracker.Scrapper.Application.InterfacesRepositories;
+
+public interface IChatRepository : IRepository<Chat>
+{
+    Task RemoveChatAsync(long id);
+    Task<Chat?> GetChatAsync(long id);
+    Task<bool> ChatExistAsync(long chatId);
+
+}
---
+++ src/LinkTracker.Scrapper.Application/InterfacesRepositories/ILinkRepository.cs
@@ -0,0 +1,10 @@
+﻿using LinkTracker.Scrapper.Domain.Models;
+
+namespace LinkTracker.Scrapper.Application.InterfacesRepositories;
+
+public interface ILinkRepository : IRepository<Link>
+{
+    Task RemoveLinkAsync(long chatId, string url);
+    Task<Link?> GetLinkAsync(long chatId, string url);
+    Task<List<Link>> GetLinksByChatAsync(long chatId, string? tag = null);
+}
