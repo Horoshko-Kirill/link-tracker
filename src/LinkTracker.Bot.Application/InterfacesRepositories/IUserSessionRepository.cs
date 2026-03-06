a500ea6c82fe9cbc2cad2d73f4b78e5fcee2ead7
kiril <khorosko15@gmail.com>
2026-03-06 09:26:25 +0300

    feat: add user session interface repository

---
+++ src/LinkTracker.Bot.Application/InterfacesRepositories/IUserSessionRepository.cs
@@ -0,0 +1,11 @@
+﻿using LinkTracker.Bot.Domain.Models;
+
+namespace LinkTracker.Bot.Application.InterfacesRepositories
+{
+    public interface IUserSessionRepository
+    {
+        Task<UserSession?> GetAsync(long chatId, CancellationToken cancellationToken = default);
+        Task SaveAsync(UserSession userSession, CancellationToken cancellationToken = default);
+        Task DeleteAsync(long chatId, CancellationToken cancellationToken = default);
+    }
+}
