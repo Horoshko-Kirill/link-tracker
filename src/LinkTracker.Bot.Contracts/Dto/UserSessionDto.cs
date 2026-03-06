c6c71e63f790a6275007be7c1896fde1ddabebec
kiril <khorosko15@gmail.com>
2026-03-06 09:21:02 +0300

    feat: add dto

---
+++ src/LinkTracker.Bot.Contracts/Dto/UserSessionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkTracker.Bot.Contracts.Dto
+{
+    public class UserSessionDto
+    {
+        public long ChatId { get; set; }
+        public string State { get; set; } = String.Empty;
+        public string? PendingLink { get; set; }
+    }
+}
