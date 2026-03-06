e6ab129cd5829af234eeb5c42606c289b54b53f0
kiril <khorosko15@gmail.com>
2026-03-06 09:43:38 +0300

    feat: add user session memory repository

--- src/LinkTracker.Bot.Application/InterfacesRepositories/IUserSessionRepository.cs
+++ src/LinkTracker.Bot.Application/InterfacesRepositories/IUserSessionRepository.cs
@@ -4,6 +4,7 @@ namespace LinkTracker.Bot.Application.InterfacesRepositories
 {
     public interface IUserSessionRepository
     {
+        Task AddAsync(UserSession userSession, CancellationToken cancellationToken = default);
         Task<UserSession?> GetAsync(long chatId, CancellationToken cancellationToken = default);
         Task SaveAsync(UserSession userSession, CancellationToken cancellationToken = default);
         Task DeleteAsync(long chatId, CancellationToken cancellationToken = default);
