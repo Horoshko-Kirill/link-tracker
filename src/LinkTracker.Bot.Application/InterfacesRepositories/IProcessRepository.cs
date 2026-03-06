ac9324399413a5aa8629a1dc1d5ee48429527d8f
kiril <khorosko15@gmail.com>
2026-03-06 22:31:36 +0300

    feat: add interfaces repository

---
+++ src/LinkTracker.Bot.Application/InterfacesRepositories/IProcessRepository.cs
@@ -0,0 +1,11 @@
+﻿using LinkTracker.Bot.Domain.Models;
+
+namespace LinkTracker.Bot.Application.InterfacesRepositories
+{
+    public interface IProcessRepository
+    {
+        Task<Process?> GetActiveProcessAsync(long chatId, CancellationToken cancellationToken = default);
+        Task CreateAsync(Process process, CancellationToken cancellationToken = default);
+        Task CompleteAsync(long chatId, CancellationToken cancellationToken = default);
+    }
+}
