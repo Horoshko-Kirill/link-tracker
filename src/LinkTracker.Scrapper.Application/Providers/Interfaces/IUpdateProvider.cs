6ae90ba6327d3bfe5c3bc654f3fbb03239eff629
kiril <khorosko15@gmail.com>
2026-03-08 11:23:44 +0300

    feat: add update provider

---
+++ src/LinkTracker.Scrapper.Application/Providers/Interfaces/IUpdateProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkTracker.Scrapper.Application.Providers.Interfaces;
+
+public interface IUpdateProvider
+{
+    bool CanHandle(Uri url);
+
+    Task<DateTimeOffset?> GetLastUpdateAsync(Uri url, CancellationToken cancellationToken = default);
+}
---
+++ src/LinkTracker.Scrapper.Application/Providers/StackOverflowUpdateProvider.cs
@@ -0,0 +1,35 @@
+﻿using LinkTracker.Scrapper.Application.InterfacesClients;
+using LinkTracker.Scrapper.Application.Providers.Interfaces;
+
+namespace LinkTracker.Scrapper.Application.Providers;
+
+public class StackOverflowUpdateProvider : IUpdateProvider
+{
+    private readonly IStackOverflowClient _stackOverflowClient;
+
+    public StackOverflowUpdateProvider(IStackOverflowClient stackOverflowClient)
+    {
+        _stackOverflowClient = stackOverflowClient;
+    }
+
+    public bool CanHandle(Uri url)
+    {
+        return url.Host.Contains("stackoverflow.com");
+    }
+
+    public async Task<DateTimeOffset?> GetLastUpdateAsync(Uri url, CancellationToken cancellationToken = default)
+    {
+        var parts = url.AbsolutePath.Split("/", StringSplitOptions.RemoveEmptyEntries);
+
+        if (parts.Length < 3)
+        {
+            return null;
+        }
+
+        var id = long.Parse(parts[1]);
+
+        var updateDate = await _stackOverflowClient.GetLastUpdateAsync(id, cancellationToken);
+
+        return updateDate;
+    }
+}
