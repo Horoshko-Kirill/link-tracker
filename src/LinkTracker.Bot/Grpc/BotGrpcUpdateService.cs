41bdeaa13d95bf302a729cfb4a2f5ba3027a2d3b
kiril <khorosko15@gmail.com>
2026-03-12 03:37:10 +0300

    feat: add bot grpc update service

---
+++ src/LinkTracker.Bot/Grpc/BotGrpcUpdateService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using LinkTracker.Bot.Contracts.Dto;
+using LinkTracker.Bot.Contracts.Grpc;
+using LinkTracker.Bot.Telegram;
+
+namespace LinkTracker.Bot.Grpc;
+
+public class BotGrpcUpdateService : BotUpdateService.BotUpdateServiceBase
+{
+    private readonly ITelegramClient _telegramClient;
+
+    public BotGrpcUpdateService(ITelegramClient telegramClient)
+    {
+        _telegramClient = telegramClient;
+    }
+
+    public override async Task<GrpcEmpty> PostUpdate(GrpcLinkUpdate update, ServerCallContext context)
+    {
+        foreach (var chatId in update.ChatIds)
+        {
+            var message = $"{update.Url} : \n {update.Description}";
+
+            await _telegramClient.SendMessageAsync(chatId, message, context.CancellationToken);
+        }
+
+        return new GrpcEmpty();
+    }
+}
