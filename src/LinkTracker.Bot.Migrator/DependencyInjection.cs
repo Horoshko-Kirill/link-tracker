5116a9871109ddf625a212cd36e1290b9ef7fddd
kiril <khorosko15@gmail.com>
2026-03-23 16:54:46 +0300

    feat: add DI

---
+++ src/LinkTracker.Bot.Migrator/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using LinkTracker.Bot.Infrastructure.Options;
+using LinkTracker.Bot.Migrator.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkTracker.Bot.Migrator;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMigrator(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+           .AddOptions<DatabaseOptions>()
+           .Bind(configuration.GetSection(DatabaseOptions.SectionName))
+           .Validate(
+               options => !string.IsNullOrWhiteSpace(options.ConnectionString),
+               "DatabaseOptions:ConnectionString must be configured")
+           .ValidateOnStart();
+
+        services.AddSingleton<IMigrationRunner, MigrationRunner>();
+
+        return services;
+    }
+}
