fdd5dd464abe7629f541c32fdbf93d8dc8d3936c
kiril <khorosko15@gmail.com>
2026-03-23 17:04:31 +0300

    feat: update migrations

--- src/LinkTracker.Bot.Migrator/Program.cs
+++ src/LinkTracker.Bot.Migrator/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using LinkTracker.Bot.Migrator;
+using LinkTracker.Bot.Migrator.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true);
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddMigrator(builder.Configuration);
+
+using var host = builder.Build();
+
+var runner = host.Services.GetRequiredService<IMigrationRunner>();
+await runner.RunAsync();
