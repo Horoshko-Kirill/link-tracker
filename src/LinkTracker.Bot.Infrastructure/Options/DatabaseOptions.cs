2f29ecd23a2b45881b79987b4a6f9dcb6830b73a
kiril <khorosko15@gmail.com>
2026-03-23 12:09:37 +0300

    feat: add bot database options

---
+++ src/LinkTracker.Bot.Infrastructure/Options/DatabaseOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace LinkTracker.Bot.Infrastructure.Options;
+
+public class DatabaseOptions
+{
+    public const string SectionName = "Database";
+    public string ConnectionString { get; set; } = string.Empty;
+}
