4e63ab519681cb616aaae5224300a2538fbc9f59
kiril <khorosko15@gmail.com>
2026-03-23 12:08:22 +0300

    feat: add database option

---
+++ src/LinkTracker.Scrapper.Infrastructure/Options/DatabaseOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkTracker.Scrapper.Infrastructure.Options;
+
+public class DatabaseOptions
+{
+    public const string SectionName = "Database";
+
+    public string ConnectionString { get; set; } = string.Empty;
+}
