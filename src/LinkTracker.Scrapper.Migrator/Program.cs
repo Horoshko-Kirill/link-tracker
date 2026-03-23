b34a60d2c5ef52c376c7f009a8187da9e7cd6a37
kiril <khorosko15@gmail.com>
2026-03-23 13:28:29 +0300

    feat: add program

--- src/LinkTracker.Scrapper.Migrator/Program.cs
+++ src/LinkTracker.Scrapper.Migrator/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using LinkTracker.Scrapper.Migrator;
+using LinkTracker.Scrapper.Migrator.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddMigrator(builder.Configuration);
+
+using var host = builder.Build();
+
+var runner = host.Services.GetRequiredService<IMigrationRunner>();
+await runner.RunAsync();
