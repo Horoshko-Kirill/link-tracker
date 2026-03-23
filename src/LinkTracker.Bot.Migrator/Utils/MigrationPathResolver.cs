9e07014b05f1c1146328756c753c214babbf9c94
kiril <khorosko15@gmail.com>
2026-03-23 16:47:47 +0300

    feat: add utils path resolver

---
+++ src/LinkTracker.Bot.Migrator/Utils/MigrationPathResolver.cs
@@ -0,0 +1,22 @@
+﻿namespace LinkTracker.Bot.Migrator.Utils;
+
+public static class MigrationPathResolver
+{
+    public static string Resolve(string basePath, string serviceName)
+    {
+        var dir = new DirectoryInfo(basePath);
+
+        while (dir != null)
+        {
+            var migrationsDir = Path.Combine(dir.FullName, "migrations", serviceName);
+            if (Directory.Exists(migrationsDir))
+            {
+                return migrationsDir;
+            }
+
+            dir = dir.Parent;
+        }
+
+        throw new InvalidOperationException($"Migrations directory 'migrations/{serviceName}' was not found.");
+    }
+}
