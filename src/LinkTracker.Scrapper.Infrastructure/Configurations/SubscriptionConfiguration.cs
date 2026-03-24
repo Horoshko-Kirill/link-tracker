071c16c246043a86d38f19d554292e7d756607c4
kiril <khorosko15@gmail.com>
2026-03-24 16:23:55 +0300

    feat: add subscription configuration

---
+++ src/LinkTracker.Scrapper.Infrastructure/Configurations/SubscriptionConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using LinkTracker.Scrapper.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LinkTracker.Scrapper.Infrastructure.Configurations;
+
+public class SubscriptionConfiguration : IEntityTypeConfiguration<Subscription>
+{
+    public void Configure(EntityTypeBuilder<Subscription> builder)
+    {
+        builder.ToTable("scrapper_subscriptions");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.ChatId)
+            .HasColumnName("chat_id")
+            .IsRequired();
+
+        builder.Property(x => x.LinkId)
+            .HasColumnName("link_id")
+            .IsRequired();
+
+        builder.HasIndex(x => new { x.ChatId, x.LinkId })
+            .IsUnique();
+
+        builder.HasOne(x => x.Chat)
+            .WithMany()
+            .HasForeignKey(x => x.ChatId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Link)
+            .WithMany(x => x.Subscriptions)
+            .HasForeignKey(x => x.LinkId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
