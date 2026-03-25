4f34dc368167c33431e445f3b76fd9324a9b89a3
kiril <khorosko15@gmail.com>
2026-03-25 14:21:20 +0300

    feat: implementation transaction unit

--- src/LinkTracker.Scrapper.Application/InterfacesCommon/IScrapperTransaction.cs
+++ src/LinkTracker.Scrapper.Application/InterfacesCommon/IScrapperTransaction.cs
@@ -1,6 +1,6 @@
 ﻿namespace LinkTracker.Scrapper.Application.InterfacesCommon;
 
-public interface IScrapperTransaction
+public interface IScrapperTransaction : IAsyncDisposable
 {
     Task CommitAsync(CancellationToken cancellationToken = default);
     Task RollbackAsync(CancellationToken cancellationToken = default);
---
+++ src/LinkTracker.Scrapper.Infrastructure/Database/Transaction/EfScrapperTransaction.cs
@@ -0,0 +1,29 @@
+﻿using LinkTracker.Scrapper.Application.InterfacesCommon;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace LinkTracker.Scrapper.Infrastructure.Database.Transaction;
+
+public class EfScrapperTransaction : IScrapperTransaction
+{
+    private readonly IDbContextTransaction _transaction;
+
+    public EfScrapperTransaction(IDbContextTransaction transaction)
+    {
+        _transaction = transaction;
+    }
+
+    public Task CommitAsync(CancellationToken cancellationToken = default)
+    {
+        return _transaction.CommitAsync(cancellationToken);
+    }
+
+    public Task RollbackAsync(CancellationToken cancellationToken = default)
+    {
+        return _transaction.RollbackAsync(cancellationToken);
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return _transaction.DisposeAsync();
+    }
+}
