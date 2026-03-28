81793175841c88c04bdaa3226675e291ac9687ff
kiril <khorosko15@gmail.com>
2026-03-29 00:08:15 +0300

    feat: add sql transaction

---
+++ src/LinkTracker.Scrapper.Infrastructure/Database/Sql/SqlSession.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+
+namespace LinkTracker.Scrapper.Infrastructure.Database.Sql;
+
+public class SqlSession
+{
+    public NpgsqlConnection?  Connection { get; set; }
+    public NpgsqlTransaction? Transaction { get; set; }
+    
+    public SqlSession(NpgsqlConnection? connection = null, NpgsqlTransaction? transaction = null)
+    {}
+    
+    public bool HasTransaction => Connection != null && Transaction != null;
+
+    public void Clear()
+    {
+        Connection =  null;
+        Transaction = null;
+    }
+}
---
+++ src/LinkTracker.Scrapper.Infrastructure/Database/Sql/SqlUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using LinkTracker.Scrapper.Application.Exceptions;
+using LinkTracker.Scrapper.Application.InterfacesCommon;
+using Npgsql;
+
+namespace LinkTracker.Scrapper.Infrastructure.Database.Sql;
+
+public class SqlUnitOfWork : IUnitOfWork
+{
+    private readonly NpgsqlDataSource _dataSource;
+    private readonly SqlSession _session;
+
+    public SqlUnitOfWork(NpgsqlDataSource dataSource, SqlSession session)
+    {
+        _dataSource = dataSource;
+        _session = session;
+    }
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task<IScrapperTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        if (_session.HasTransaction)
+        {
+            throw new TransactionExceptions("Transaction already started");
+        }
+        
+        var connection = await _dataSource.OpenConnectionAsync(cancellationToken);
+        var transaction = await connection.BeginTransactionAsync(cancellationToken);
+
+        _session.Connection = connection;
+        _session.Transaction = transaction;
+        
+        return new SqlScrapperTransaction(_session);
+    }
+}
