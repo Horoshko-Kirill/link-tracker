ac6beaeb23989dfef1a56377bb22b13997a8729c
kiril <khorosko15@gmail.com>
2026-03-31 00:44:58 +0300

    feat: add pagination options

--- src/LinkTracker.Bot.Application/DI/Extensions.cs
+++ src/LinkTracker.Bot.Application/DI/Extensions.cs
@@ -3,15 +3,19 @@ using LinkTracker.Bot.Application.Factories.Interfaces;
 using LinkTracker.Bot.Application.Handlers.ActionHandlers;
 using LinkTracker.Bot.Application.Handlers.Interfaces;
 using LinkTracker.Bot.Application.InterfacesServices;
+using LinkTracker.Bot.Application.Options;
 using LinkTracker.Bot.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace LinkTracker.Bot.Application.DI;
 
 public static class Extensions
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<PaginationOptions>(configuration.GetSection(PaginationOptions.SectionName));
+        
         services.AddTransient<IActionItemFactory, TrackActionItemFactory>();
         services.AddTransient<IActionItemFactory, UntrackActionItemFactory>();
 
---
+++ src/LinkTracker.Bot.Application/Options/PaginationOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace LinkTracker.Bot.Application.Options;
+
+public class PaginationOptions
+{
+    public const string SectionName = "Pagination";
+    public int PageSize { get; set; }
+}
