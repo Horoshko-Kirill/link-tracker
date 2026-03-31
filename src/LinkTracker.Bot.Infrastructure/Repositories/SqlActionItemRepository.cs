f53cea98c8eb526cfb94fc27ad77b8e09366f112
kiril <khorosko15@gmail.com>
2026-03-31 16:50:56 +0300

    feat: implement sql process repository

---
+++ src/LinkTracker.Bot.Infrastructure/Repositories/SqlActionItemRepository.cs
@@ -0,0 +1,23 @@
+﻿using LinkTracker.Bot.Application.Common.Pagination;
+using LinkTracker.Bot.Application.InterfacesRepositories;
+using LinkTracker.Bot.Domain.Models;
+
+namespace LinkTracker.Bot.Infrastructure.Repositories;
+
+public class SqlActionItemRepository : IActionItemRepository
+{
+    public Task AddAsync(ActionItem action, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ActionItem?> GetLastAsync(long processId, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ActionItem>> GetPageAsync(long processId, PageRequest pageRequest, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
