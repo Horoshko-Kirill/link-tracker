b7883e381f96700c47ad063fca088532dd928aa8
kiril <khorosko15@gmail.com>
2026-04-06 16:08:54 +0300

    feat: add new update entity

---
+++ src/LinkTracker.Scrapper.Domain/Models/UpdateEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkTracker.Scrapper.Domain.Models;
+
+public class UpdateEvent : Entity
+{
+    public long LinkId { get; set; }
+    public Link Link { get; set; } = null!;
+    public string EventType { get; set; } = String.Empty;
+    public string Source { get; set; } = String.Empty;
+    public string Title { get; set; } =  String.Empty;
+    public string Author { get; set; } =  String.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public string Preview { get; set; } = String.Empty;
+    public DateTimeOffset DetectedAt { get; set; }
+}
