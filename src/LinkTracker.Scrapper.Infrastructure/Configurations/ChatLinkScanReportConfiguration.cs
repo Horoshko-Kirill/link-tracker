01ccf2adaff80b1c7a9df6dd6bcd323abe03ace6
kiril <khorosko15@gmail.com>
2026-04-10 14:07:19 +0300

    feat: add configuration

--- src/LinkTracker.Scrapper.Infrastructure/Configurations/ChatLinkScanReportConfiguration.cs
+++ src/LinkTracker.Scrapper.Infrastructure/Configurations/ChatLinkScanReportConfiguration.cs
@@ -1,6 +1,52 @@
-﻿namespace LinkTracker.Scrapper.Infrastructure.Configurations;
+﻿using LinkTracker.Scrapper.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class ChatLinkScanReportConfiguration
+namespace LinkTracker.Scrapper.Infrastructure.Configurations;
+
+public class ChatLinkScanReportConfiguration : IEntityTypeConfiguration<ChatLinkScanReport>
 {
-    
+    public void Configure(EntityTypeBuilder<ChatLinkScanReport> builder)
+    {
+        builder.ToTable("scrapper_link_scan_report");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .HasColumnName("id");
+        
+        builder.Property(x => x.ChatId)
+            .HasColumnName("chat_id")
+            .IsRequired();
+        
+        builder.Property(x => x.ScanStartedAt)
+            .HasColumnName("scan_started_at")
+            .IsRequired();
+        
+        builder.Property(x => x.ScanFinishedAt)
+            .HasColumnName("scan_finished_at")
+            .IsRequired();
+        
+        builder.Property(x => x.FailedCount)
+            .HasColumnName("failed_count")
+            .IsRequired();
+        
+        builder.Property(x => x.Message)
+            .HasColumnName("message")
+            .IsRequired();
+
+        builder.Property(x => x.SentAt)
+            .HasColumnName("sent_at");
+        
+        builder.Property(x => x.Status)
+            .HasColumnName("status")
+            .HasConversion<int>()
+            .IsRequired();
+
+        builder.HasOne<Chat>()
+            .WithMany()
+            .HasForeignKey(x => x.ChatId);
+
+    }
 }
