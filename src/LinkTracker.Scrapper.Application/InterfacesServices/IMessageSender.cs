2ef3e5f51d178a9edf8e498e7a097cdb2a10a3bf
kiril <khorosko15@gmail.com>
2026-04-10 20:50:43 +0300

    feat: add message sender

---
+++ src/LinkTracker.Scrapper.Application/InterfacesServices/IMessageSender.cs
@@ -0,0 +1,8 @@
+﻿using LinkTracker.Bot.Contracts.Dto;
+
+namespace LinkTracker.Scrapper.Application.InterfacesServices;
+
+public interface IMessageSender
+{
+    Task SendAsync(LinkUpdate linkUpdate, CancellationToken cancellationToken = default);
+}
---
+++ src/LinkTracker.Scrapper.Application/Services/HttpMessageSender.cs
@@ -0,0 +1,20 @@
+﻿using LinkTracker.Bot.Contracts.Dto;
+using LinkTracker.Scrapper.Application.InterfacesClients;
+using LinkTracker.Scrapper.Application.InterfacesServices;
+
+namespace LinkTracker.Scrapper.Application.Services;
+
+public class HttpMessageSender : IMessageSender
+{
+    private readonly IBotClient _botClient;
+
+    public HttpMessageSender(IBotClient botClient)
+    {
+        _botClient = botClient;
+    }
+    
+    public Task SendAsync(LinkUpdate linkUpdate, CancellationToken cancellationToken = default)
+    {
+        return _botClient.PostUpdateAsync(linkUpdate, cancellationToken);
+    }
+}
