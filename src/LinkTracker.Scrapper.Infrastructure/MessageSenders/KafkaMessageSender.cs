434b9e0e3a75777b34d405f10ede4df8a33cabba
kiril <khorosko15@gmail.com>
2026-04-29 14:23:05 +0300

    feat: add kafka message service

---
+++ src/LinkTracker.Scrapper.Infrastructure/MessageSenders/KafkaMessageSender.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using Confluent.Kafka;
+using LinkTracker.Bot.Contracts.Dto;
+using LinkTracker.Scrapper.Application.InterfacesServices;
+using LinkTracker.Scrapper.Infrastructure.Options;
+using Microsoft.Extensions.Options;
+
+namespace LinkTracker.Scrapper.Infrastructure.MessageSenders;
+
+public class KafkaMessageSender : IMessageSender
+{
+    private readonly IProducer<string, string> _producer;
+    private readonly KafkaOptions _options;
+
+    public KafkaMessageSender(IProducer<string, string> producer, IOptions<KafkaOptions> options)
+    {
+        _producer = producer;
+        _options = options.Value;
+    }
+    
+    public async Task SendAsync(LinkUpdate linkUpdate, CancellationToken cancellationToken = default)
+    {
+        var json = JsonSerializer.Serialize(linkUpdate);
+
+        var message = new Message<string, string> { Key = linkUpdate.Url, Value = json };
+        
+        await _producer.ProduceAsync(_options.Topic, message, cancellationToken);
+    }
+}
---
+++ src/LinkTracker.Scrapper.Infrastructure/Options/KafkaOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace LinkTracker.Scrapper.Infrastructure.Options;
+
+public class KafkaOptions
+{
+    public string BootstrapServers { get; set; } = null!;
+    public string Topic { get; set; } = null!;
+}
