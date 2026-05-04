7a3dbad0cf7f7b4269aefacb18170694a5f86407
kiril <khorosko15@gmail.com>
2026-05-04 19:27:40 +0300

    refactor: delete comments

--- src/LinkTracker.Bot.Infrastructure/DI/Extensions.cs
+++ src/LinkTracker.Bot.Infrastructure/DI/Extensions.cs
@@ -21,7 +21,6 @@ public static class Extensions
         services.AddRepository(dbOptions);
 
         services.AddTelegramClient();
-        /*
         services.AddKafkaProducer();
         
         services.AddScoped<ILinkUpdateMessageProcessor, LinkUpdateMessageProcessor>();
@@ -29,7 +28,6 @@ public static class Extensions
         services.AddSingleton<IDeadLetterQueueProducer, DeadLetterQueueProducer>();
         
         services.AddHostedService<LinkUpdateKafkaConsumer>();
-        */
         return services;
     }
 }
